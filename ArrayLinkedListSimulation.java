45abac017bcf349458c2f8a7235cd23f6e2ce434
PrinceCanencia <canenciaprincemj@smcbi.edu.ph>
2025-09-03 22:28:13 +0800

    Add files via upload

---
+++ ArrayLinkedListSimulation.java
@@ -0,0 +1,63 @@
+// Prince MJ D. Canencia
+// BSIT 2B
+// September 3,2025
+
+public class ArrayLinkedListSimulation {
+    private int[] elements = new int[5];
+    private int size = 0;
+
+    // Add element to end; resize if full
+    public void add(int value) {
+        if (size == elements.length) {
+            int[] temp = new int[elements.length * 2];
+            System.arraycopy(elements, 0, temp, 0, elements.length);
+            elements = temp;
+        }
+        elements[size++] = value;
+    }
+
+    // Remove and return first element; shift left
+    public int poll() {
+        if (size == 0) return -1;
+        int val = elements[0];
+        System.arraycopy(elements, 1, elements, 0, --size);
+        return val;
+    }
+
+    // Return first element without removing
+    public int peek() {
+        return size == 0 ? -1 : elements[0];
+    }
+
+    // Remove and return last element
+    public int pop() {
+        return size == 0 ? -1 : elements[--size];
+    }
+
+    // Display elements
+    public void display() {
+        System.out.print(size == 0 ? "List is empty." : "Current List: ");
+        for (int i = 0; i < size; i++) System.out.print(elements[i] + " ");
+        System.out.println();
+    }
+
+    // Test the class
+    public static void main(String[] args) {
+        ArrayLinkedListSimulation list = new ArrayLinkedListSimulation();
+
+        System.out.println("Adding elements:");
+        for (int i = 1; i <= 5; i++) { list.add(i * 10); list.display(); }
+
+        System.out.println("\nPeek: " + list.peek());
+        System.out.println("\nPoll: " + list.poll()); list.display();
+        System.out.println("\nPop: " + list.pop()); list.display();
+
+        System.out.println("\nRemoving all:");
+        while (list.peek() != -1) { System.out.println("Polled: " + list.poll()); list.display(); }
+
+        System.out.println("\nTesting empty:");
+        System.out.println("Peek: " + list.peek());
+        System.out.println("Poll: " + list.poll());
+        System.out.println("Pop: " + list.pop());
+    }
+}
